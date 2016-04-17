6a3f938a60429994ede0bc805bce0f570f5a5010
Anunay <i.anunay@tcs.com>
2016-04-17 12:47:57 +0100

    Manage Route stubs UI

--- src/routing.jsx
+++ src/routing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Store from 'store';
-import {PageHeader, Accordion, Grid, Row, Col, Button, Label, Panel, Modal} from 'react-bootstrap';
+import {PageHeader, Accordion, Grid, Row, Col, Button, Label, Panel} from 'react-bootstrap';
 import RoutingManager from './routingManager.jsx';
 
 class Routing extends React.Component {
@@ -9,12 +9,13 @@ class Routing extends React.Component {
     this.updateState = this.updateState.bind(this);
     this.addRoute = this.addRoute.bind(this);
     this.deleteRoute = this.deleteRoute.bind(this);
-    this.manageStubs = this.manageStubs.bind(this);
     this.updateStubs = this.updateStubs.bind(this);
     Store.Routing = this;
 
     this.state = {
-      routes: []
+      routes: [],
+      stublist: [],
+      dynamiclist: []
     };
   };
   componentWillMount(){
@@ -25,7 +26,7 @@ class Routing extends React.Component {
     Store.stubUpdate.removeListner(this.updateStubs);
   }
   updateStubs(){
-    this.setState({stublist: Store.stubConfig.stubs});
+    this.setState({stublist: Store.stubConfig.stubs, dynamiclist: Store.stubConfig.dynamic});
   }
   addRoute() {
     this.setState({count: this.state.routes.push({newRoute: true, stubs:[], dynamicStubs: []})});
@@ -36,9 +37,6 @@ class Routing extends React.Component {
   }
   updateState(){
     this.setState({routes: Store.config.routes, count: Store.config.routes.length});
-  }
-  manageStubs(){
-    this.setState({manageStubsShow: true});
   }
   render(){
     let PanelArray = [];
@@ -54,36 +52,24 @@ class Routing extends React.Component {
       );
       PanelArray.push(
         <Panel bsStyle="success" header={header} key={i + 1} eventKey={i + 1}>
-          <RoutingManager {...route} formValid={!route.newRoute} manageStubs={this.manageStubs} delete={this.deleteRoute.bind(this, i)}/>
+          <RoutingManager {...route} formValid={!route.newRoute}
+            delete={this.deleteRoute.bind(this, i)} stublist={this.state.stublist}
+            dynamiclist={this.state.dynamiclist} />
         </Panel>
       )
     }.bind(this));
     PanelArray.push(<Button style={{"marginTop": "10px"}} key='add' onClick={this.addRoute} bsStyle="success" bsSize="small">Add Route</Button>);
     return (
-      <div>
-        <Grid>
-          <Row>
-            <Col xs={12} md={6}>
-              <PageHeader>Manage routes</PageHeader>
-              <Accordion>
-                {PanelArray}
-              </Accordion>
-            </Col>
-          </Row>
-        </Grid>
-        <Modal show={this.state.manageStubsShow} bsSize="small" aria-labelledby="contained-modal-title-sm">
-          <Modal.Header>
-            <Modal.Title id="contained-modal-title-sm">Manage stubs</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h4>All Stubs</h4>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button bsStyle="primary">Add</Button>
-            <Button onClick={() => this.setState({manageStubsShow: false})}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
+      <Grid>
+        <Row>
+          <Col xs={12} md={6}>
+            <PageHeader>Manage routes</PageHeader>
+            <Accordion>
+              {PanelArray}
+            </Accordion>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
--- src/routingManager.jsx
+++ src/routingManager.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Store from 'store';
-import {Input, ButtonToolbar, ButtonGroup, Button, Grid, Row, Col} from 'react-bootstrap';
+import {Input, ButtonToolbar, ButtonGroup, Button, Grid, Row, Col, Modal} from 'react-bootstrap';
 
 class RoutingManager extends React.Component {
   constructor(props, context) {
@@ -9,10 +9,10 @@ class RoutingManager extends React.Component {
     this.validate = this.validate.bind(this);
     this.updateRoute = this.updateRoute.bind(this);
     this.deleteRoute = this.deleteRoute.bind(this);
-    this.updateStubs = this.updateStubs.bind(this);
     this.manageStubs = this.manageStubs.bind(this);
+    this.stubCheck = this.stubCheck.bind(this);
 
-    this.state = Object.assign({formValid: false, old_route: this.props.route, stublist: (Store.stubConfig && Store.stubConfig.stubs) || []}, this.props);
+    this.state = Object.assign({formValid: false, old_route: this.props.route}, this.props);
   };
   validate(state){
     let validity = state.route && state.route.charAt(0) == '/' && state.route.charAt(state.route.length - 1) == '/'
@@ -40,10 +40,21 @@ class RoutingManager extends React.Component {
     }
   }
   manageStubs() {
-    this.props.manageStubs();
+    this.setState({manageStubsShow: true});
+  }
+  stubCheck(stub, type) {
+    let routeStubList = this.state[type].slice(),
+        stubIndex = routeStubList.indexOf(stub);
+    if (stubIndex > -1) {
+      routeStubList.splice(stubIndex)
+    } else {
+      routeStubList.push(stub)
+    }
+    this.setState({stubs: routeStubList});
   }
   render(){
-    let routeInput;
+    let routeInput,
+        stublist;
     if (this.state.handle == "proxy") {
       routeInput = <Input type="text" label="Proxy to" value={this.state.proxy} placeholder="http://someaddress.com/" onChange={this.handleChange.bind(this, "proxy")} onBlur={this.handleChange.bind(this, "proxy")}/>;
     } else if (this.state.handle == "stub") {
@@ -57,6 +68,9 @@ class RoutingManager extends React.Component {
         <a href="javscript:;" onClick={this.manageStubs}>Manage stubs</a>
         </div>
       );
+      stublist = this.props.stublist.map((stub, i) => {
+        return <Input key={i} type="checkbox" label={stub.name} onChange={this.stubCheck.bind(this, stub.name, "stubs")} checked={this.state.stubs.indexOf(stub.name) > -1} />
+      })
     } else if (this.state.handle == "dynamicStub") {
       let options = this.state.dynamicStubs.map((stub, i) => <option key={i} value={stub}>{stub}</option>);
       routeInput = (
@@ -68,30 +82,48 @@ class RoutingManager extends React.Component {
         <a href="javscript:;" onClick={this.manageStubs}>Manage dynamic stubs</a>
         </div>
       );
+      stublist = this.props.dynamiclist.map((stub, i) => {
+        return <Input key={i} type="checkbox" label={stub.name} onChange={this.stubCheck.bind(this, stub.name, "dynamic")} checked={this.state.dynamicStubs.indexOf(stub.name) > -1} />
+      })
     }
     return (
-      <Grid>
-        <Row>
-          <Col xs={6} md={6}>
-            <Input type="text" help="Starts and ends with a '/'" value={this.state.route} label="Route" placeholder="/route/" onChange={this.handleChange.bind(this, "route")} onBlur={this.handleChange.bind(this, "route")} />
-            <div style={{"marginTop": "25px"}}>
-              {routeInput}
-            </div>
-          </Col>
-          <Col xs={6} md={6} pullRight={true}>
-            <div style={{'paddingLeft': '75px'}}>
-              <label>Handle</label>
-              <Input type="radio" label="Proxy" value="proxy" checked={this.state.handle == "proxy"} onChange={this.handleChange.bind(this, "handle")}/>
-              <Input type="radio" label="Stub" value="stub" checked={this.state.handle == "stub"} onChange={this.handleChange.bind(this, "handle")}/>
-              <Input type="radio" label="Dynamic Stub" value="dynamicStub" checked={this.state.handle == "dynamicStub"} onChange={this.handleChange.bind(this, "handle")}/>
-            </div>
-            <ButtonToolbar style={{"margin": "25px 0 0 75px"}}>
-              <Button bsStyle="primary" onClick={this.updateRoute} disabled={!this.state.formValid}>Update</Button>
-              <Button bsStyle="danger" onClick={this.deleteRoute}>Delete</Button>
-            </ButtonToolbar>
-          </Col>
-        </Row>
-      </Grid>
+      <div>
+        <Grid>
+          <Row>
+            <Col xs={6} md={6}>
+              <Input type="text" help="Starts and ends with a '/'" value={this.state.route} label="Route" placeholder="/route/" onChange={this.handleChange.bind(this, "route")} onBlur={this.handleChange.bind(this, "route")} />
+              <div style={{"marginTop": "25px"}}>
+                {routeInput}
+              </div>
+            </Col>
+            <Col xs={6} md={6} pullRight={true}>
+              <div style={{'paddingLeft': '75px'}}>
+                <label>Handle</label>
+                <Input type="radio" label="Proxy" value="proxy" checked={this.state.handle == "proxy"} onChange={this.handleChange.bind(this, "handle")}/>
+                <Input type="radio" label="Stub" value="stub" checked={this.state.handle == "stub"} onChange={this.handleChange.bind(this, "handle")}/>
+                <Input type="radio" label="Dynamic Stub" value="dynamicStub" checked={this.state.handle == "dynamicStub"} onChange={this.handleChange.bind(this, "handle")}/>
+              </div>
+              <ButtonToolbar style={{"margin": "25px 0 0 75px"}}>
+                <Button bsStyle="primary" onClick={this.updateRoute} disabled={!this.state.formValid}>Update</Button>
+                <Button bsStyle="danger" onClick={this.deleteRoute}>Delete</Button>
+              </ButtonToolbar>
+            </Col>
+          </Row>
+        </Grid>
+        <Modal show={this.state.manageStubsShow} bsSize="small" aria-labelledby="contained-modal-title-sm">
+          <Modal.Header>
+            <Modal.Title id="contained-modal-title-sm">Manage stubs</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Choose Stubs</h4>
+            {stublist}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="primary">Update</Button>
+            <Button onClick={() => this.setState({manageStubsShow: false})}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     )
   }
 }
